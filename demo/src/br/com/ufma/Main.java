470c6eee5c3c717671fc7b31e69949a0918525e1
Luan A. <barkon32@tuta.io>
2024-11-19 00:18:06 -0300

    feat: implement operator, expression and parse in Parser

--- demo/src/br/com/ufma/Main.java
+++ demo/src/br/com/ufma/Main.java
@@ -1,34 +1,13 @@
 package br.com.ufma;
 
-import java.io.File;
-
-import javax.swing.JFileChooser;
-
 
 public class Main {
-    
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("digite o path do arquivo que voce deseja fazer analise lexica");
-        
-        JFileChooser fileCarrega = new JFileChooser();
-        fileCarrega.setDialogTitle("qual o arquivo?");
-        
-        int userSelection = fileCarrega.showOpenDialog(null);
-        
-        
-        if (userSelection == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileCarrega.getSelectedFile();
-            String path = selectedFile.getAbsolutePath();
-            System.out.println("Arquivo selecionado: " + path);
-            
-            Lexico.geracaoLexica(selectedFile);
-            
-        } else {
-        	System.out.println("nao foi possivel selecionar o arquivo ");
-        }
-        
-        
+        String input = "1+2-3+4";
+        Parser p = new Parser (input.getBytes());
+        p.parse();
+
     }
+
 }
--- demo/src/br/com/ufma/Parser.java
+++ demo/src/br/com/ufma/Parser.java
@@ -2,17 +2,19 @@ package br.com.ufma;
 
 public class Parser {
 
-    private final Scanner scan;
+    private final Scanner scanner;
 
     private char currentToken;
 
+    private String[] operatorsList = {"+", "-"};
+
     public Parser(byte[] input) {
-        scan = new Scanner(input);
-        currentToken = scan.nextToken();
+        scanner = new Scanner(input);
+        currentToken = scanner.nextToken();
     }
 
     private void nextToken () {
-        currentToken = scan.nextToken();
+        currentToken = scanner.nextToken();
     }
 
     private void match(char token) {
@@ -23,4 +25,35 @@ public class Parser {
         }
     }
 
+    void digit () {
+        if (Character.isDigit(currentToken)) {
+            System.out.println("push " + currentToken);
+            match(currentToken);
+        } else {
+            throw new Error("syntax error");
+        }
+    }
+
+    void operator() {
+        if (currentToken == TokenType.PLUS.getChar()) {
+            match(TokenType.PLUS.getChar());
+            digit();
+            System.out.println("add");
+            operator();
+        } else if (currentToken == TokenType.MINUS.getChar()) {
+            match(TokenType.MINUS.getChar());
+            digit();
+            System.out.println("sub");
+            operator();
+        }
+    }
+
+    void expression() {
+        digit();
+        operator();
+    }
+
+    public void parse () {
+        expression();
+    }
 }
