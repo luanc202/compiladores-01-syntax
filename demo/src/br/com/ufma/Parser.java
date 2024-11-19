9f80fa9c6fb2ef3b108e35937c18da00b79c67a9
Luan A. <barkon32@tuta.io>
2024-11-19 00:06:40 -0300

    feat: implement token matcher in Paraser

--- demo/src/br/com/ufma/Parser.java
+++ demo/src/br/com/ufma/Parser.java
@@ -2,12 +2,25 @@ package br.com.ufma;
 
 public class Parser {
 
-    private Scanner scan;
+    private final Scanner scan;
+
     private char currentToken;
 
     public Parser(byte[] input) {
         scan = new Scanner(input);
         currentToken = scan.nextToken();
+    }
+
+    private void nextToken () {
+        currentToken = scan.nextToken();
+    }
+
+    private void match(char token) {
+        if (currentToken == token) {
+            nextToken();
+        }else {
+            throw new Error("syntax error: invalid token");
+        }
     }
 
 }
