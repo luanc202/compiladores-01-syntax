f15f26934c435d226dac4b8d5c4e9991efee8c44
Luan A. <barkon32@tuta.io>
2024-11-21 17:18:20 -0300

    change input

--- src/br/com/ufma/Main.java
+++ src/br/com/ufma/Main.java
@@ -4,7 +4,7 @@ package br.com.ufma;
 public class Main {
 
     public static void main(String[] args) {
-        String input = "1+2-3+4";
+        String input = "89+508-7+99";
         Parser p = new Parser (input.getBytes());
         p.parse();
 
