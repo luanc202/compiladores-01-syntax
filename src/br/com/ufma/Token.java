f32c514ecce23a2fe5d7599bb3362f735fd09552
Luan A. <barkon32@tuta.io>
2024-11-21 17:08:38 -0300

    create Token class type

--- src/br/com/ufma/Token.java
+++ src/br/com/ufma/Token.java
@@ -2,4 +2,15 @@ package br.com.ufma;
 
 public class Token {
 
+    final TokenType type;
+    final String lexeme;
+
+    public Token(TokenType type, String lexeme) {
+        this.type = type;
+        this.lexeme = lexeme;
+    }
+
+    public String toString() {
+        return "<" + type + ">" + lexeme + "</" + type + ">";
+    }
 }
